72441cfa47e556857040a5ce30270c69f86b930f
Abdullah Asghar <dulakhan024@gmail.com>
2023-03-29 14:06:58 +0200

    Authentication almost done

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import greetingReducer from '../features/greetingSlice';
+import logger from 'redux-logger';
+import sessionReducer from './features/sessions/sessionSlice';
+// import rightDisplayReducer from '../redux/RSidebarReducers/RSidebarReducers';
+// import ItemListReducers from '../redux/ItemList/ItemListReducers';
+// import userReducer from '../redux/user/userSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    counter: greetingReducer,
+    session: sessionReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export default store;
